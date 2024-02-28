0aae466f6bec3cc5a790649e4a296cbbc1297004
İbrahim Sezer <154599100+ibrahimsezer01@users.noreply.github.com>
2024-02-28 12:59:14 +0300

    app.js

--- Tarayıcıların Depolama alanları/Session Storage/app.js
+++ Tarayıcıların Depolama alanları/Session Storage/app.js
@@ -1,28 +1,24 @@
-// Session Storage web sayfası kapatıldığı anda verilerin silindiği depolama biçimidir (Genellikle kullanılmaz)
+// Session Storage web sayfası kapatıldığı anda verilerin silindiği depolama biçimidir
 
 // Inputlar
-
 const addkey = document.querySelector("#addkey");
 const addvalue = document.querySelector("#addvalue");
 const deletekey = document.querySelector("#deletekey");
 
 
 // Butonlar
-
 const add = document.querySelector("#add");
 const del = document.querySelector("#delete");
 const clear = document.querySelector("#clear");
 
 
-// Click özellikleri
-
+// Click Events
 add.addEventListener("click",addItem);
 del.addEventListener("click",delItem);
 clear.addEventListener("click",clearItems);
 
 
-// Fonksiyonlara atama
-
+// Funcs
 function addItem(e) {
     sessionStorage.setItem(addkey.value,addvalue.value);
 }
@@ -35,4 +31,4 @@ function clearItems(e) {
     sessionStorage.clear();
 }
 
-// sayfa kapanıp tekrardan açıldığında bunların hepsi silinmiş olur
+// sayfa kapanıp tekrardan açıldığında bunların hepsi silinmiş olur --- sadece kullanıcının o anki oturumana bağlılık gösterirler
